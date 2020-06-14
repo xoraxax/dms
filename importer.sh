8def540c7b5b03111a1b55f38ecd5e614b858b1f
Ingolf Wagner <contact@ingolf-wagner.de>
2020-06-14 14:26:16 +0200

    refactor: update importer.sh

--- importer.sh
+++ importer.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-set -x
-mtime=`stat -c '%Y' $1`
-newname=`date -d "@$mtime" -Iseconds | tr :+ __`
-newname="$2/$newname.pdf"
-mv "$1" "$newname"
+#set -x
+
+FILE="$1"
+OUTPUT="$2"
+
+mtime=$(stat -c '%Y' "$FILE")
+newname=$(date -d "@$mtime" -Iseconds | tr :+ __)
+newname="$OUTPUT/$newname.pdf"
+mv "$FILE" "$newname"
 pdfsandwich -lang deu+eng "$newname"
 mv "$newname" "${newname%.pdf}.orig.pdf"
 mv "${newname%.pdf}_ocr.pdf" "$newname"
-pdfinfo "$newname" | grep Pages: | sed 's/[^0-9]*//' > "${newname%.pdf}.count"
+pdfinfo "$newname" | grep Pages: | sed 's/[^0-9]*//' >"${newname%.pdf}.count"
 convert -thumbnail x600 "${newname%.pdf}.orig.pdf[0]" "${newname%.pdf}.png"
 pdftotext "$newname"
